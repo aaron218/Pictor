0c198b5805102c4f984a6d98e1bf38ae1d7b7187
aaron218 <12155829@qq.com>
2014-12-18 22:46:49 +0800

    4

--- src/main/java/net/theneverland/java/Filter/BasicFilter.java
+++ src/main/java/net/theneverland/java/Filter/BasicFilter.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 /**
  * Created by Administrator on 2014/12/18 0018.
  * 过滤器的基本操作
+ *
+ * 与Servlet的Filter类似，但是目前的设计是只有一个单向的处理过程，而不需要进行返回处理
  */
 public interface BasicFilter {
 
---
+++ src/main/java/net/theneverland/java/Filter/FilterRequest.java
@@ -0,0 +1,7 @@
+package net.theneverland.java.Filter;
+
+/**
+ * Created by Administrator on 2014/12/18 0018.
+ */
+public interface FilterRequest {
+}
