b8644ca9f6e5b29e47211245d15760d417a97a03
aaron218 <12155829@qq.com>
2014-12-19 18:17:20 +0800

    5.1

--- src/main/java/net/theneverland/java/Filter/SimpleFilter/package-info.java
+++ src/main/java/net/theneverland/java/Filter/SimpleFilter/package-info.java
@@ -1,5 +1,6 @@
 /**
  * Created by aaron on 12/19/2014.
  * 使用责任链模式开发的简单过滤器主要考虑性能问题而不是易用性和扩展性问题
+ * http://www.java3z.com/cwbwebhome/article/article8/81230.html
  */
 package net.theneverland.java.Filter.SimpleFilter;
